1fbc123647654fe3bd65545e7b47fc71a108dfcd
georgejeffery <georgejeffery@gmail.com>
2019-05-21 12:12:59 +0100

    Sorted display methods

--- parser.rb
+++ parser.rb
@@ -4,29 +4,37 @@ class LogParser
   def initialize(path)
     @log = File.readlines(path)
     @pages = @log.map { |line| line.strip.gsub(/\s.+/, "") }.group_by(&:itself).transform_values(&:count)
+    @uniques = {}
   end
 
-  def allpageviews
-    output = Hash[@pages.sort_by { |key, value| value }.reverse!]
+  def print
+    pages = Hash[@pages.sort_by { |key, value| value }.reverse!]
     puts "Total Views"
-    output.each do |line|
+    pages.each do |line|
       puts "#{line[0]}, #{line[1]} views"
+    end
+    unique = Hash[@uniques.sort_by { |key, value| value }.reverse!]
+    puts "-"
+    puts "Unique Views"
+    unique.each do |line|
+      puts "#{line[0]}, #{line[1]} unique views"
     end
   end
 
   def uniqueviews
-    arry = []
     @pages.each_key do |key|
+      arry = []
       @log.each do |line|
         if line.strip.gsub(/\s.+/, "") == key
           arry.push (line)
         end
       end
-      puts "#{key} has #{arry.count} unique visits"
+      # byebug
+      @uniques[key] = arry.uniq.count
     end
   end
 
   parser = LogParser.new(*ARGV)
-  parser.allpageviews
   parser.uniqueviews
+  parser.print
 end
